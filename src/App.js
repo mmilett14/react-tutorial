1b6fed4218acf68a762fe89408f034fc84ab9b0f
Mike <mmilett14@gmail.com>
2021-04-14 16:03:35 -0500

    began component section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Mike's Kichen</h1>
+    </header>
+  )
+}
+
+function Footer() {
+  <p>It's true!</p>
+}
+
+function Main() {
+  return (
+    <section>
+      <p> We serve the most delicious food around.</p>
+    </section>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
