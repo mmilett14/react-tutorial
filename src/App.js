627f562ecbc4f11267671b74e7048930972324dc
Mike <mmilett14@gmail.com>
2021-04-15 16:45:56 -0500

    finished chapter 4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion])
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`)
-  }, [secondary])
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
 
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}</h1>
-      <button onClick={() => setEmotion("Stoked")}>
-        Make Stoke
-      </button>
-      <button onClick={() => setEmotion("crabby")}>
-        Make Crabby
-      </button>
-      <button onClick={() => setEmotion("Frustrated")}>
-        Frustrate
-      </button>
-      <button onClick={() => setEmotion("Enthusiastic")}>
-        Enthuse
-      </button>
+      <input type="checkbox"
+       value={checked}
+        onChange={toggle}
+      />
+      <p>{checked ? "checked" : "not checked" }</p>
     </>
   );
-
-  }
+}
 
 export default App;
