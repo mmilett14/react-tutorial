027776774f0e8d8ca11a9f29de6a74a2c7b9cbb8
Mike <mmilett14@gmail.com>
2021-04-15 16:37:11 -0500

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
 import './App.css';
-import logo from "./logo.svg";
-
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
-}
-
-function Main(props) {
-  return (
-    <section>
-      <p> We serve the most {props.adjective} food around.</p>
-      <img 
-        src={logo} 
-        height={200} 
-        alt="react logo"
-      />
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  )
-}
-
-function Footer(props) {
-  return (
-  <p>Copyright {props.year}</p>
-  )
-}
-
-const dishes = [
-  "Mac n Chz",
-  "curry",
-  "tofu"
-]
-
-const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
-
-dishes.map((dish) => console.log(dish));
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion])
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary])
+
   return (
-    <div className="App">
-      <Header name="Meaya" />
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()} />
-    </div>
+    <>
+      <h1>Current emotion is {emotion} and {secondary}</h1>
+      <button onClick={() => setEmotion("Stoked")}>
+        Make Stoke
+      </button>
+      <button onClick={() => setEmotion("crabby")}>
+        Make Crabby
+      </button>
+      <button onClick={() => setEmotion("Frustrated")}>
+        Frustrate
+      </button>
+      <button onClick={() => setEmotion("Enthusiastic")}>
+        Enthuse
+      </button>
+    </>
   );
-}
+
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 
