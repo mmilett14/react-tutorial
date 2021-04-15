0efb7833e92c8e1f6b596ecf8c99f846e695d0ba
Mike <mmilett14@gmail.com>
2021-04-14 21:38:25 -0500

    finished ch.3 - react components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import './App.css';
+import logo from "./logo.svg";
 
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>Mike's Kichen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   )
 }
 
-function Footer() {
-  <p>It's true!</p>
-}
-
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p> We serve the most delicious food around.</p>
+      <p> We serve the most {props.adjective} food around.</p>
+      <img 
+        src={logo} 
+        height={200} 
+        alt="react logo"
+      />
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
     </section>
   )
 }
 
+function Footer(props) {
+  return (
+  <p>Copyright {props.year}</p>
+  )
+}
+
+const dishes = [
+  "Mac n Chz",
+  "curry",
+  "tofu"
+]
+
+const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
+
+dishes.map((dish) => console.log(dish));
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Meaya" />
+      <Main adjective="amazing" dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
-ReactDOM.render(
-  <ul>
-    <li>aye</li>
-    <li>yo</li>
-    <li>bruh</li>
-  </ul>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
