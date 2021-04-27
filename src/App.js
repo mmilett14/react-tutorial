8d666cd4466b9bbc6436afa7a0461d69c0a518d5
Mike <mmilett14@gmail.com>
2021-04-27 14:44:08 -0500

    started testing

--- src/App.js
+++ src/App.js
@@ -3,36 +3,13 @@ import React, { useState, useEffect} from "react";
 
 // https://api.github.com/users/mmilett14
 
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+function App() {
 
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response) => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
-  if(data) {
     return (
     <div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img alt={data.login} src={data.avatar_url} />
+      <h1>Hello React Testing Library</h1>
     </div>
     );
   }
-
-return <div>No User Available</div>;
-}
 
 export default App;
