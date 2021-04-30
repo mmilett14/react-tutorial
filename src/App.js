7915426b1736f876efde8c237795f77251ff8993
Mike <mmilett14@gmail.com>
2021-04-30 16:08:53 -0500

    Initalized React Router:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
-import React, { useState, useEffect} from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import {
+  Home,
+  About,
+  Events,
+  Contact,
+  Whoops404,
+  Services,
+  CompanyHistory,
+  Location
+} from "./pages"
 
 // https://api.github.com/users/mmilett14
 
@@ -7,7 +18,17 @@ function App() {
 
     return (
     <div>
-      <h1>Hello React Testing Library</h1>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}>
+        <Route path="services" element={<Services />} />
+        <Route path="history" element={<CompanyHistory />} />
+        <Route path="location" element={<Location />} />
+        </Route>
+        <Route path="/events" element={<Events />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="*" element={<Whoops404 />}/>
+      </Routes>
     </div>
     );
   }
